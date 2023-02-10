98c27143518bfc146a6105e86285534207e2a4aa
saraneuie <sneuensc@umich.edu>
2023-02-10 15:13:27 -0500

    first code

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,13 @@
-window.alert("This is an alert! JavaScript is running!");
+// this creates a function named "fight"
+
+var playerName = window.prompt("What is your robot's name?");
+
+console.log(playerName);
+
+function fight() {
+    window.alert("The fight has begun!");
+  }
+
+
+
+ //fight (); 
